efed60233b0c2d16585c6c7b0febfdae4ab20c2c
Liubavaa <92572643+Liubavaa@users.noreply.github.com>
2022-10-23 18:02:22 +0300

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,16 +1,16 @@
 package flower.store;
 
-import java.util.*;
+import java.util.LinkedList;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter @Getter
 public class FlowerBucket {
-    private final LinkedList<FlowerPack> FlowerPacks = new LinkedList<>();
+    private final LinkedList<FlowerPack> flowerPacks = new LinkedList<>();
 
-    public void add(FlowerPack pack) { FlowerPacks.add(pack); }
+    public void add(FlowerPack pack) { flowerPacks.add(pack); }
 
     public double getPrice() {
-        return FlowerPacks.stream().mapToDouble(FlowerPack::getPrice).sum();
+        return flowerPacks.stream().mapToDouble(FlowerPack::getPrice).sum();
     }
 }
