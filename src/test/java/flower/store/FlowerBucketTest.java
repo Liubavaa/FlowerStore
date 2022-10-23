115df32d8a1f13861e115b16677684b39383d72d
Liubavaa <92572643+Liubavaa@users.noreply.github.com>
2022-10-23 16:40:18 +0300

    Update FlowerBucketTest.java

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -1,9 +1,9 @@
-package lab5.FlowerStore.src.test.java.flower.store;
+package FlowerStore.src.test.java.flower.store;
 
-import lab5.FlowerStore.src.main.java.flower.store.Flower;
-import lab5.FlowerStore.src.main.java.flower.store.FlowerBucket;
-import lab5.FlowerStore.src.main.java.flower.store.FlowerPack;
-import lab5.FlowerStore.src.main.java.flower.store.Rose;
+import FlowerStore.src.main.java.flower.store.Flower;
+import FlowerStore.src.main.java.flower.store.FlowerBucket;
+import FlowerStore.src.main.java.flower.store.FlowerPack;
+import FlowerStore.src.main.java.flower.store.Rose;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
